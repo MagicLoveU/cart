c3bbb3c1e56ada86940f0b1839b29a349ab41f07
Administrator <Administrator@172.26.211.69>
2020-04-15 17:59:13 +0800

    demo

---
+++ src/dao/ProductDAO.java
@@ -0,0 +1,98 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Product;
+
+public class ProductDAO {
+
+	public static void main(String[] args) {
+
+		System.out.println(new ProductDAO().ListProduct().size());
+
+	}
+
+	public Product getProduct(int id) {
+		Product result = null;
+
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			Connection c = DriverManager.getConnection("jdbc:sqlserver://127.0.0.1:1433;databaseName=cart", "sa", "sa");
+
+			String sql = "select * from product where id = ?";
+
+			PreparedStatement ps = c.prepareStatement(sql);
+            ps.setInt(1, id);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				result = new Product();
+				result.setId(id);
+				result.setName(rs.getString(2));
+				result.setPrice(rs.getFloat(3));
+
+			}
+
+			ps.close();
+
+			c.close();
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public List<Product> ListProduct() {
+		List<Product> products = new ArrayList<Product>();
+
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			Connection c = DriverManager.getConnection("jdbc:sqlserver://127.0.0.1:1433;databaseName=cart", "sa", "sa");
+
+			String sql = "select * from product order by id desc";
+
+			PreparedStatement ps = c.prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				Product product = new Product();
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				float price = rs.getFloat(3);
+
+				product.setId(id);
+				product.setName(name);
+				product.setPrice(price);
+				products.add(product);
+
+			}
+
+			ps.close();
+
+			c.close();
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return products;
+	}
+}
